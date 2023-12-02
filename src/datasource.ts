14ce466ea9ea827440c106f9e7420e7660555960
Александр Федякин <a.fedyakin@pravo.tech>
2023-12-03 01:33:55 +0300

    use env for configure connection

--- src/datasource.ts
+++ src/datasource.ts
@@ -3,12 +3,12 @@ import { Template } from './entities/template';
 import { Field } from './entities/field';
 import { Document } from './entities/document';
 import { DocumentValue } from './entities/document_values';
+import 'dotenv/config';
 
 export const dataSource = new DataSource({
 	type: 'postgres',
-	url: 'postgresql://postgres:postgres@127.0.0.1:5432/onlanta',
+	url: process.env.PG_URL,
 	applicationName: 'ONLANTA',
 	schema: 'Documents',
 	entities: [Template, Field, Document, DocumentValue],
-	// synchronize: true,
 });
